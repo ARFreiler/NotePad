4aa2ecec1c74c1424b02a433a3907e45507f2906
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-05-13 21:24:19 -0400

    Added function to get index.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require("fs");
 
 const app = express();
 
@@ -7,9 +8,16 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./index.html"));
+});
 
+
+
+// Section
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
+//Section
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
